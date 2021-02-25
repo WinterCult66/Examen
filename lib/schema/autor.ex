ff8abeee3bd5837850b4fd93e8321f462d6737ee
Kevin Alexander Rodriguez Rodriguez <kerodriguez@personalsoft.com.co>
2021-02-25 13:09:31 -0500

    add valdiation to changeset

--- lib/schema/autor.ex
+++ lib/schema/autor.ex
@@ -9,8 +9,20 @@ defmodule Examen.Autor do
   end
 
   def changeset(autor, attrs) do
+    IO.puts("llego")
     autor
     |> cast(attrs, [:name])
     |> validate_required([:name])
+    |> validate_not_nil([:name, :name])
+  end
+
+  def validate_not_nil(changeset, fields) do
+    Enum.reduce(fields, changeset, fn field, changeset ->
+      if get_field(changeset, field) == nil do
+        add_error(changeset, field, "nil")
+      else
+        changeset
+      end
+    end)
   end
 end
--- test/examen_test.exs
+++ test/examen_test.exs
@@ -1,49 +1,61 @@
 defmodule Examen.ExamenTest do
   use ExUnit.Case
   use Examen.DataCase
-  #alias Examen.HelperAutor
+  # alias Examen.HelperAutor
   alias Examen.HelperBook
 
   describe "#Test to validate the join with 6 data when is Success" do
     setup do
       autor = insert(:autor)
-      book = insert(:book, autor_id: autor.id )
+      book = insert(:book, autor_id: autor.id)
 
       autor2 = insert(:autor)
-      book2 = insert(:book, autor_id: autor2.id )
+      book2 = insert(:book, autor_id: autor2.id)
 
       autor3 = insert(:autor)
-      book3 = insert(:book, autor_id: autor3.id )
+      book3 = insert(:book, autor_id: autor3.id)
 
       autor4 = insert(:autor)
-      book4 = insert(:book, autor_id: autor4.id )
+      book4 = insert(:book, autor_id: autor4.id)
 
       autor5 = insert(:autor)
-      book5 = insert(:book, autor_id: autor5.id )
+      book5 = insert(:book, autor_id: autor5.id)
 
-      {:ok, [autor: autor , book: book, autor: autor2 , book: book2, autor: autor3 , book: book3, autor: autor4 , book: book4, autor: autor5 , book: book5]}
+      {:ok,
+       [
+         autor: autor,
+         book: book,
+         autor: autor2,
+         book: book2,
+         autor: autor3,
+         book: book3,
+         autor: autor4,
+         book: book4,
+         autor: autor5,
+         book: book5
+       ]}
     end
 
     test "#get data", %{
       autor: _,
       book: _
     } do
-      joins =  HelperBook.get_books_with_author
-        assert length(joins) == 6
+      joins = HelperBook.get_books_with_author()
+      assert length(joins) == 6
     end
   end
 
-  describe "# " do
+  describe "insert author" do
     setup do
-      autor = insert(:autor)
+      autor = insert(:autor, name: nil)
+      {:ok, autor: autor}
     end
 
-    test "#get data", %{
-      autor: _,
-      book: _
+    test "#get data 2", %{
+      autor: autor
     } do
-      joins =  HelperBook.get_books_with_author
-        assert length(joins) == 6
+      IO.inspect(autor, label: ">>>>>>>>>>>>>>")
+      assert true
     end
   end
 end
